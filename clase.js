23381b715dc710272f1a3f600c2053e4484f7dbf
IgnacioOreiro <ignaciooreiro3@outlook.es>
2022-06-14 23:45:50 -0300

    Primera entrega del proyecto final

--- clase.js
+++ clase.js
@@ -1,20 +1,55 @@
+let medida = prompt("")
 
-let cantidadHamburguesas = prompt("Ingrese el numero de hamburguesas que desea")
-let quierePapas = prompt("Quiere papas?")
-for (let index = 0; index < cantidadHamburguesas; index++) {
-let ingrediene1 = prompt("Porfavor ingrese el primer ingrediente de su hamburguesa");
-let ingrediene2 = prompt("Porfavor ingrese el segundo ingrediente de su hamburguesa");
-let ingrediene3 = prompt("Porfavor ingrese el tercer ingrediente de su hamburguesa");
-let ingrediene4 = prompt("Porfavor ingrese el cuarto ingrediente de su hamburguesa");
+sillon2 = "170 a 180" 
+sillon3 = "180 a 190"
+sillon4 = "190 a 200"
+sillon5 = "200"
 
-if ((ingrediene1 != "") && (ingrediene2 != "") && (ingrediene3 != "") && (ingrediene4 != "")) {
-    let pedido = "Su hamburguesa tiene los sigientes ingredientes: " + "1) " + ingrediene1 + " " + "2) " + ingrediene2 + " " + "3) " + ingrediene3 + " " + "4) " + ingrediene4 + "<br>";
-    document.write(pedido);
-} else {
-    document.write("Porfavor recuerde que es necesario ingresar todos los ingredientes");
+calor1 = "El color de su sillon es verde"
+color2 = "El color de su sillon es rojo"
+color3 = "El color de su sillon es blanco"
+color4 = "El color de su sillon es offwhite"
+
+let sillon1 =  prompt("Seleccione la medida de su sillon 170 a 180 de 180 a 190 o 190 a 200");
+while ((sillon1 != 'ESC') && (sillon2 = "170 a 180")  && (sillon3 = "180 a 190")  && (sillon4 = "190 a 200") && (sillon5 = "200")) {
+    switch (sillon1) {
+        case medida:
+            alert(valor1);
+            break;
+        case medida2:
+            alert(valor2);
+            break;
+        case medida3:
+            alert(valor3);
+            break;
+        case medida4:
+            alert(valor4);
+            break;
+        default:
+            alert("Error");
+            break;
+    }
+
+    let color =  prompt("Seleccione el color de su sillon");
+while (color != 'ESC' && (medida = "170 a 180")  && (medida2 = "180 a 190")  && (medida3 = "190 a 200") && (medida4 = "200")) {
+    switch (color) {
+        case "verde":
+            alert();
+            break;
+        case "rojo":
+            alert("El valor de su sillon es de $21850");
+            break;
+        case "blanco":
+            alert("El valor de su sillon es de $23400");
+            break;
+        case "offwhite":
+            alert("El valor de su sillon es de $24450");
+            break;
+        default:
+            alert("Error");
+            break;
+    }
+    color = prompt("Seleccione el color de su sillon");
 }
 }
 
-while((quierePapas !="no") && (quierePapas != "No") && (quierePapas != "NO")){
-    document.write("Aqui tiene sus papas y su " );
-}
