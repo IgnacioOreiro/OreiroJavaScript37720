6ff9e4983b6726f0378c56135adbf078beb87715
IgnacioOreiro <ignaciooreiro3@outlook.es>
2022-06-21 20:48:14 -0300

    Primera entrega del proyecto final

--- clase.js
+++ clase.js
@@ -1,55 +1,31 @@
-let medida = prompt("")
 
-sillon2 = "170 a 180" 
-sillon3 = "180 a 190"
-sillon4 = "190 a 200"
-sillon5 = "200"
 
-calor1 = "El color de su sillon es verde"
-color2 = "El color de su sillon es rojo"
-color3 = "El color de su sillon es blanco"
-color4 = "El color de su sillon es offwhite"
-
-let sillon1 =  prompt("Seleccione la medida de su sillon 170 a 180 de 180 a 190 o 190 a 200");
-while ((sillon1 != 'ESC') && (sillon2 = "170 a 180")  && (sillon3 = "180 a 190")  && (sillon4 = "190 a 200") && (sillon5 = "200")) {
-    switch (sillon1) {
-        case medida:
-            alert(valor1);
-            break;
-        case medida2:
-            alert(valor2);
-            break;
-        case medida3:
-            alert(valor3);
-            break;
-        case medida4:
-            alert(valor4);
-            break;
-        default:
-            alert("Error");
-            break;
+function solicitarNombre(){
+    let nombreIngresado = prompt("Ingrese su nombre porfavor!");
+    alert("El nombre ingresado es " + nombreIngresado)
     }
-
-    let color =  prompt("Seleccione el color de su sillon");
-while (color != 'ESC' && (medida = "170 a 180")  && (medida2 = "180 a 190")  && (medida3 = "190 a 200") && (medida4 = "200")) {
-    switch (color) {
-        case "verde":
-            alert();
-            break;
-        case "rojo":
-            alert("El valor de su sillon es de $21850");
-            break;
-        case "blanco":
-            alert("El valor de su sillon es de $23400");
-            break;
-        case "offwhite":
-            alert("El valor de su sillon es de $24450");
-            break;
-        default:
-            alert("Error");
-            break;
-    }
-    color = prompt("Seleccione el color de su sillon");
+    let medidas;
+    function solicitarMedidas(){
+    medidas = prompt("Porfavor ingrese las medidas de su sillon!");
+    alert("Estas fueron las medidas ingresadas "+ medidas);
+}
+    function entregarPrecios(datoSolicitado){
+    if ((datoSolicitado >= "150") && (datoSolicitado <= "160")){
+    alert("El precio de su sillon es de $22500");
+}else if((datoSolicitado >= "170") && (datoSolicitado <= "180")){
+    alert("El precio de su sillon es de $24500");
+}else if((datoSolicitado >= "180") && (datoSolicitado <= "190")){
+    alert("El precio de su sillon es de $26500");
+}else if((datoSolicitado >= "200") && (datoSolicitado <= "210")){
+    alert("El precio de su sillon es de $28500");
 }
 }
+
+    solicitarNombre();
+    solicitarMedidas();
+    entregarPrecios(medidas);
+
+
+
+
 
